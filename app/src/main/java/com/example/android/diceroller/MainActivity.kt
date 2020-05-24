cac66a04e4096991eb3ac6cc6ace0330f22387ba
Anand Balan <abalan@abalan-mac.lan>
2020-05-24 23:46:06 +0530

    Add dice images on roll
    Use the previously added dice images and update the dice value to corresponding dice image when the user rolls the dice.

--- app/src/main/java/com/example/android/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/android/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.android.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.widget.ImageView
 import com.example.android.diceroller.databinding.ActivityMainBinding
 import java.util.*
 
@@ -13,10 +14,21 @@ class MainActivity : AppCompatActivity() {
         with(ActivityMainBinding.inflate(LayoutInflater.from(this))) {
             setContentView(root)
             btnRoll.setOnClickListener {
-                tvDiceValue.text = rollDice().toString()
+                rollDice(diceIv)
             }
         }
     }
 
-    private fun rollDice(): Int = Random().nextInt(6) + 1
+    private fun rollDice(diceImageView: ImageView) {
+        val diceImageRes = when(Random().nextInt(6) + 1) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            6 -> R.drawable.dice_6
+            else -> R.drawable.empty_dice
+        }
+        diceImageView.setImageResource(diceImageRes)
+    }
 }
